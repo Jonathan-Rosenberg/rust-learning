653bf6e62643d32155041ef8aad6f47fbc41c962
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2022-12-22 20:49:23 +0200

    convert string input to unsigned int 32 and compare it to the random nuber

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io; // importing the 'io' module from the standard library (package) 'std'
 use rand::Rng; // 'use' == import. 'Rng' is a trait that must be in scope to use random number generators methods.
+use std::cmp::Ordering;
 
 // to read more about any crate that we import to the project, run `cargo doc --open` and choose the crate you're interested in.
 
@@ -22,6 +23,17 @@ fn main() {
     io::stdin() // calling 'stdin' returns a 'Stdin' instnace that has methods such as 'read_line'
         .read_line(&mut guess) // like variables, references are immutable by deafult in rust. Therefore, if we want a reference to be changed we must declare it as 'mut'.
         .expect("Failed to read line");
+
+    // the ':' tells Rust we;ll annotate the variable's type. Parse realizes that the parsed expression should be u32.
+    let guess: u32 = guess.trim().parse().expect("Please type a numer!");
+
+    // match expression is much like a switch case expression but the terminology is a bit different:
+    // 'match' is made out of `arms`. An `arm` consists of a `pattern` to match against.
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You Winnnnnn!"),
+    }
     
     println!("You guessed: {guess}");
 }
