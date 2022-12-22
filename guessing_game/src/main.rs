e0e2c98a54de41e3e1d8d1ef8a3ceabd84919b29
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2022-12-22 20:52:15 +0200

    break the game when winning

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -33,7 +33,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You Winnnnnn!"),
+            Ordering::Equal => {
+                println!("You Winnnnnn!");
+                break;
+            },
         }
         
         println!("You guessed: {guess}");
