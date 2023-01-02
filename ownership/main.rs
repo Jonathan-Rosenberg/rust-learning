673b904ec533b836681aa6db40d3291e604d1c65
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-01-02 16:54:51 +0200

    add reference mutation

--- ownership/main.rs
+++ ownership/main.rs
@@ -1,12 +1,19 @@
 fn main() {
-    let s1: String = String::from("what?!");
+    let mut s1: String = String::from("what?!");
 
     let len = calculate_len(&s1); // passing reference
 
     println!("Length of '{}' is {}.", s1, len);
+
+    mutating(&mut s1);
+    println!("Mutated string: '{}'", s1);
 }
 
 fn calculate_len(s: &String) -> usize { // s is a reference to a String
     s.len()
 } // Here, s goes out of scope. But because it does not have ownership of what
   // it refers to, it is not dropped.
+
+fn mutating(v: &mut String) {
+    v.push_str("- added this string");
+}
